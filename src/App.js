764c2f25c237714e3ecfdde712a25003a8a806e9
Timothy Efraim Hotasi <timothyefraim35@gmail.com>
2022-11-30 05:01:09 +0700

    Modify App.js to test the input components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import './App.css';
 import Button from './components/Button';
+import Input from './components/Input';
+import TextArea from './components/TextArea';
+import Select from './components/Select';
 
+/* Components Playground */
 function App() {
   return (
-    <>
     <div className="App">
       <header className="App-header">
         <Button text={'Long text here her her her'}/>
+        <Input label={'Nama'} placeholderText={'Masukan nama Anda'} type={'text'} />
+        <Input label={'Password'} placeholderText={'Masukan password Anda'} type={'password'}/>
+        <TextArea label={'Deskripsi'}></TextArea>
+        <Select label={'Permainan'} items={[1,2,3]}></Select>
       </header>
     </div>
-    <form>
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Submit" />
-</form>
-    </>
   )
 }
 
