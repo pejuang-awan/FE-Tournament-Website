8873b0e258e8db9859766c63972d17c764943d87
haradi <zulfahri123@gmail.com>
2022-11-29 14:25:43 +0700

    finish button maybe

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from './components/button';
+import Button from './components/Button';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <Button text={'Long text here her her her'}/>
       </header>
     </div>
-  );
+    <form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+    </>
+  )
 }
 
 export default App;
