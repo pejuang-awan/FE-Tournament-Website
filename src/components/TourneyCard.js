7782ad345494c47e8336bbcc56c185dab8ff3a4c
haradi <zulfahri123@gmail.com>
2022-12-01 20:34:57 +0700

    implements TourneyList page, modify Tourney card, and create imgUrl constant

--- src/components/TourneyCard.js
+++ src/components/TourneyCard.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import '../static/css/components/TourneyCard.css';
+import { RiTeamFill } from "react-icons/ri";
+import { FiClock } from "react-icons/fi";
 
-export default function tourneyCard({ name, description, participants, quota, deadline, imgUrl }) {
+export default function tourneyCard({ name, description, participants, quota, deadline, imgUrl}) {
     return (
         <div className='tourney-card'>
             <div className='tourney-card-container'>
-                <p className='tourney-card-name'>{name}</p>
+                <div className='tourney-card-banner'>
+                    <div 
+                    className='tourney-banner-img'
+                    style={{backgroundImage: `url(${imgUrl})`}}
+                    />
+                    <p className='tourney-card-name'><span>{name}</span></p>
+                </div>
                 <p className='tourney-card-desc'>{description}</p>
                 <div className='tourney-card-bottom'>
                     <div className='tourney-card-participants'>
-                        <img className='tourney-card-logo' src={imgUrl}></img>
+                        <RiTeamFill className='tourney-card-logo'/>
                         <p>Tim terdaftar</p>
                         <p>{participants}/{quota}</p>
                     </div>
                     <div className='tourney-card-deadline'>
-                        <img className='tourney-card-logo' src={imgUrl}></img>
+                        <FiClock className='tourney-card-logo'/>
                         <p>Tenggat Daftar</p>
                         <p>{deadline} hari</p>
                     </div>
--- src/pages/TourneyList.js
+++ src/pages/TourneyList.js
@@ -1,9 +1,100 @@
 import React from 'react';
+import '../static/css/pages/TourneyList.css';
+import Navbar from '../components/Navbar';
+import TourneyCard from '../components/TourneyCard';
+import { imgURL } from '../constant/imgURL';
 
 export default function TourneyList() {
+    const dummy = {
+        "gameType":3 
+    }
+    const bannerImgUrl = `http://drive.google.com/uc?export=view&id=${imgURL[dummy.gameType].TOURNEY_BANNER}`
+    const cardImgUrl = `http://drive.google.com/uc?export=view&id=${imgURL[dummy.gameType].TOURNEY_CARD_HEADER}`
+
     return (
-        <React.Fragment>
-            <p>Tournament List page</p>
-        </React.Fragment>
+        <div>
+            <Navbar/>
+            <div className='header-container'>
+                <div
+                className='banner'
+                style={{backgroundImage: `url(${bannerImgUrl})`}}
+                />
+                <h1><span>Mobile Legends: Bang Bang</span></h1>
+            </div>
+            <div className='body-container'>
+                <TourneyCard 
+                    name="M4 World" 
+                    description="Tempat RRQ akan kembali mengbadut yagesya."
+                    imgUrl={cardImgUrl}
+                    participants={1}
+                    quota={20}
+                    deadline={5}
+                ></TourneyCard>
+                <TourneyCard 
+                    name="M4 World" 
+                    description="Tempat RRQ akan kembali mengbadut yagesya."
+                    imgUrl={cardImgUrl}
+                    participants={1}
+                    quota={20}
+                    deadline={5}
+                ></TourneyCard>
+                <TourneyCard 
+                    name="M4 World" 
+                    description="Tempat RRQ akan kembali mengbadut yagesya."
+                    imgUrl={cardImgUrl}
+                    participants={1}
+                    quota={20}
+                    deadline={5}
+                ></TourneyCard>
+                <TourneyCard 
+                    name="M4 World" 
+                    description="Tempat RRQ akan kembali mengbadut yagesya."
+                    imgUrl={cardImgUrl}
+                    participants={1}
+                    quota={20}
+                    deadline={5}
+                ></TourneyCard>
+                <TourneyCard 
+                    name="M4 World" 
+                    description="Tempat RRQ akan kembali mengbadut yagesya."
+                    imgUrl={cardImgUrl}
+                    participants={1}
+                    quota={20}
+                    deadline={5}
+                ></TourneyCard>
+                <TourneyCard 
+                    name="M4 World" 
+                    description="Tempat RRQ akan kembali mengbadut yagesya."
+                    imgUrl={cardImgUrl}
+                    participants={1}
+                    quota={20}
+                    deadline={5}
+                ></TourneyCard>
+                <TourneyCard 
+                    name="M4 World" 
+                    description="Tempat RRQ akan kembali mengbadut yagesya."
+                    imgUrl={cardImgUrl}
+                    participants={1}
+                    quota={20}
+                    deadline={5}
+                ></TourneyCard>
+                <TourneyCard 
+                    name="M4 World" 
+                    description="Tempat RRQ akan kembali mengbadut yagesya."
+                    imgUrl={cardImgUrl}
+                    participants={1}
+                    quota={20}
+                    deadline={5}
+                ></TourneyCard>
+                <TourneyCard 
+                    name="M4 World" 
+                    description="Tempat RRQ akan kembali mengbadut yagesya."
+                    imgUrl={cardImgUrl}
+                    participants={1}
+                    quota={20}
+                    deadline={5}
+                ></TourneyCard>
+            </div>
+        </div>
     )
 }
